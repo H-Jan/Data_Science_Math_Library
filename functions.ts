bbf9b3fc5c1c4f03b871e4eabed6691c976e4916
Hani J <hanijandali@gmail.com>
2022-06-10 19:35:30 -0700

    Addition of IQR, and outliers function

--- functions.ts
+++ functions.ts
@@ -29,6 +29,61 @@ function getMedian(arr: number[]): number{
     return ((arr[halved - 1] + arr[halved]) / 2.0);
 }
 
+function getFirstQuartile(arr: number[]): number{
+    const values: number[] = arr.slice().sort( (a: number, b: number) => a - b);
+    
+    if ((values.length / 4) % 1 === 0){
+        var q1 = 1/2 * (values[(values.length / 4)] + values[(values.length / 4) + 1]);
+    } else {
+        var q1 = values[Math.floor(values.length / 4 + 1)];
+    }
+    return q1
+}
+
+function getThirdQuartile(arr: number[]): number{
+    const values: number[] = arr.slice().sort( (a: number, b: number) => a - b);
+    
+    if ((values.length / 4) % 1 === 0){
+        var q3 = 1/2 * (values[(values.length * (3 / 4))] + values[(values.length* (3 / 4)) + 1]);
+    } else {
+        var q3 = values[Math.floor(values.length * (3/ 4)+ 1)];
+    }
+    return q3
+}
+
+function getIQR(arr: number[]): number{
+    const x1: number = getFirstQuartile(arr);
+    const x3: number = getThirdQuartile(arr);
+    const iqr: number = x3 - x1;
+    return iqr
+}
+
+function getOutliers(arr: number[]): number[]{
+    const values: number[] = arr.slice().sort( (a: number, b: number) => a - b);
+
+    const quartile1: number = getFirstQuartile(arr);
+    const quartile3: number = getThirdQuartile(arr);
+    const iqr: number = getIQR(arr)
+    const maxValue: number = quartile3 + iqr * 1.5;
+    const minValue: number = quartile1 - iqr * 1.5;
+
+    const outliers: number[] = values.filter((x) => (x >= minValue) && (x <= maxValue));
+
+    return outliers
+}
+
+function getStdDev(arr: number[]): number{
+    const array_length: number = arr.length;
+    const mean: number = getMean(arr);
+    let temp_array: number[] = arr.map((el) => {
+        return (el - mean) ** 2;
+    })
+
+    let total: number = arr.reduce((accuracy: number, current: number) => accuracy + current, 0);
+
+    return Math.sqrt(total / arr.length);
+}
+
 function MSE(pred_val: number[], obs_val: number[]): number{
     let error: number = 0;
     for (let i: number = 0; i < pred_val.length; i += 1) {
